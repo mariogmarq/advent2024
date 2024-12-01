df2d854ef3b276abc28d1fb2134377d01dfb3dad
Mario García Márquez <mariogarciamarq@gmail.com>
2024-12-01 11:00:06 +0100

    day01 refactored

--- src/day01.zig
+++ src/day01.zig
@@ -1,10 +1,8 @@
 const std = @import("std");
 
-pub fn part_one(numbers: []u8, alloc: std.mem.Allocator) anyerror!i32 {
+fn read_lists(numbers: []u8, alloc: std.mem.Allocator) anyerror![2]std.ArrayList(i32) {
     var first_list = std.ArrayList(i32).init(alloc);
     var second_list = std.ArrayList(i32).init(alloc);
-    defer first_list.deinit();
-    defer second_list.deinit();
 
     var lines = std.mem.tokenize(u8, numbers, "\n");
     while (lines.next()) |line| {
@@ -15,6 +13,16 @@ pub fn part_one(numbers: []u8, alloc: std.mem.Allocator) anyerror!i32 {
         try first_list.append(first);
         try second_list.append(second);
     }
+
+    return [_]std.ArrayList(i32){ first_list, second_list };
+}
+
+pub fn part_one(numbers: []u8, alloc: std.mem.Allocator) anyerror!i32 {
+    const lists = try read_lists(numbers, alloc);
+    var first_list = lists[0];
+    var second_list = lists[1];
+    defer first_list.deinit();
+    defer second_list.deinit();
 
     std.mem.sort(i32, first_list.items, {}, comptime std.sort.asc(i32));
     std.mem.sort(i32, second_list.items, {}, comptime std.sort.asc(i32));
@@ -27,31 +35,25 @@ pub fn part_one(numbers: []u8, alloc: std.mem.Allocator) anyerror!i32 {
     return total_distance;
 }
 
+pub fn create_counter(comptime T: type, values: []const T, alloc: std.mem.Allocator) !std.AutoHashMap(T, i32) {
+    var counter = std.AutoHashMap(T, i32).init(alloc);
+    for (values) |value| {
+        const times_appeared = counter.get(value) orelse 0;
+        try counter.put(value, times_appeared + 1);
+    }
+
+    return counter;
+}
+
 pub fn part_two(numbers: []u8, alloc: std.mem.Allocator) anyerror!i32 {
-    var first_list = std.ArrayList(i32).init(alloc);
-    var second_list = std.ArrayList(i32).init(alloc);
+    const lists = try read_lists(numbers, alloc);
+    var first_list = lists[0];
+    var second_list = lists[1];
     defer first_list.deinit();
     defer second_list.deinit();
 
-    var lines = std.mem.tokenize(u8, numbers, "\n");
-    while (lines.next()) |line| {
-        var numbers_iterator = std.mem.splitSequence(u8, line, "   ");
-        const first = try std.fmt.parseInt(i32, numbers_iterator.next().?, 10);
-        const second = try std.fmt.parseInt(i32, numbers_iterator.next().?, 10);
-
-        try first_list.append(first);
-        try second_list.append(second);
-    }
-
-    var similarity_score = std.AutoHashMap(i32, i32).init(alloc);
+    var similarity_score = try create_counter(i32, second_list.items, alloc);
     defer similarity_score.deinit();
-    for (second_list.items) |num| {
-        if (similarity_score.contains(num)) {
-            try similarity_score.put(num, similarity_score.get(num).? + 1);
-        } else {
-            try similarity_score.put(num, @as(u32, 1));
-        }
-    }
 
     var score = @as(i32, 0);
     for (first_list.items) |num| {
